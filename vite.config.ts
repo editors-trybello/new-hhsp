476df9f02cc1684f17424438b44d0280120c322e
editors-trybello <editors@trybello.com>
2026-04-17 04:39:19 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+   base: "/new-hhsp/",
   plugins,
   resolve: {
     alias: {
